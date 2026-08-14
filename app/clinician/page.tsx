9f6ac0800a95354e0377ddf357f6295f758a960d
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2026-08-14 06:46:02 +0100

    Use simplified clinician workspace

--- app/clinician/page.tsx
+++ app/clinician/page.tsx
@@ -1,6 +1,5 @@
-import ClinicianWorkspace from '../../components/ClinicianWorkspace';
+import ClinicianWorkspaceV2 from '../../components/ClinicianWorkspaceV2';
 
 export default function ClinicianPage() {
-  return <ClinicianWorkspace />;
+  return <ClinicianWorkspaceV2 />;
 }
-
